0854a8c44cf0c2eb96c579aa17aea93bf05a1d7a
Harsha Naga K <harshanagak@gmail.com>
2024-06-12 13:06:10 +0530

    install parcell

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-
-const h1=React.createElement("h1",{},"I am H1 tag");
-const child = React.createElement("div",{id:"child"},h1)
-const parent = React.createElement("div",{id:"parent"},child)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "This is Namaste React🚀"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
 
+console.log(parent);
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!")
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+
 
 root.render(parent);
